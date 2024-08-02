812bb853882bf60bff2ee7649a0f069537bc0ca0
Ben Rehmann <brehmann@me.com>
2024-08-02 09:51:24 -0400

    variables and functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,38 @@ import Image from "next/image";
 
 export default function Home() {
 
-  let greeting = 'hello world';
+  // lets declare a variable
 
-  const humidity = 79;
+  // Example: const variable: type = value
 
+  let greeting: string = 'hello world';
+  const humidity: number = 79;
+  let isRaining: boolean = false;
+  let endTime: Date
+  let awesomeVariable: any = 'this type any could be anything...'
+
+  // if you dont identify a type, typescript will assume it's type
+  
+
+  // declaring arrays...
+  // two ways
+  let numberArray: number[] = [1, 2, 3, 4];
+  let stringArray: Array<string> = ['one', 'two', 'three'];
+
+
+  // function creation
+  // must define the types of your params
+  function add(a: number, b: number) {
+    return a + b;
+  }
+  // typescript is checking your params
+  const addError = add(3, '4');
+  const addNailedIt = add(3, 4);
+
+
+
+
+  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
